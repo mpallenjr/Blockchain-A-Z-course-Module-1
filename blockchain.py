7cc6642464471cebc04d0b67519242da4d6912ea
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2022-02-02 16:19:30 -0600

    previous block function

--- blockchain.py
+++ blockchain.py
@@ -15,15 +15,17 @@ class Blockchain:  #Class will include Genesis Block, Initalize a Chain(init fun
         self.create_block(proof = 1, previous_hash = '0') #must put the 0 in quotes because the SHA256 encryption only accepts strings. Create block function
 
     def create_block(self, proof, previous_hash):
-        block = (                                           #The block can represent any data you chose. This is the data chosen for this particular exercise.
+        block = {                                           #The block can also have a data key for this particular exercise there is no additional data. What will be represented in postman
             'index': len(self.chain) + 1,                   #The index will return the length of the blockchain
             'timestamp': str(datetime.datetime.now()),      #The timestamp will atuomatically apply date and time to the block when it is created. Must be in a string format. 
-            'proof': proof,                                 #A function to be called on later.
-            'previous_hash': previous_hash)                 #Links the previous block to this block
+            'proof': proof,                                 #A function to be called on later. "The proof found or solved."
+            'previous_hash': previous_hash                  #Links the previous block to this block
+            }                 
         delf.chain.append(block)                            #appends the block, elongating 
         return block                                        #returns the created block as a value
 
-
+    def get_previous_block(self):                           #function that returns the last block of the chain
+        return self.chain[-1]
 
 # Part 2 - Mining our Blockchain
 
