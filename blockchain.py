b1c92afc05e7ce4766303b3b6df0c1b6fd50fec0
“mpallenjr”git config --global user.email “mpallenjnr@gmail.com” <mikeallen@Michaels-MBP.attlocal.net>
2022-02-02 16:11:18 -0600

    add create_block function with notes.

--- blockchain.py
+++ blockchain.py
@@ -14,7 +14,14 @@ class Blockchain:  #Class will include Genesis Block, Initalize a Chain(init fun
         self.chain = [] #creating an empty list/array function for the blockchain
         self.create_block(proof = 1, previous_hash = '0') #must put the 0 in quotes because the SHA256 encryption only accepts strings. Create block function
 
-
+    def create_block(self, proof, previous_hash):
+        block = (                                           #The block can represent any data you chose. This is the data chosen for this particular exercise.
+            'index': len(self.chain) + 1,                   #The index will return the length of the blockchain
+            'timestamp': str(datetime.datetime.now()),      #The timestamp will atuomatically apply date and time to the block when it is created. Must be in a string format. 
+            'proof': proof,                                 #A function to be called on later.
+            'previous_hash': previous_hash)                 #Links the previous block to this block
+        delf.chain.append(block)                            #appends the block, elongating 
+        return block                                        #returns the created block as a value
 
 
 
